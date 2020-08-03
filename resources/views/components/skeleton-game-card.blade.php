a4c0b06cb62652f096225697c03cc9954da4125f
Bonge <ikaranjah@zoho.com>
2020-08-03 13:12:24 +0300

    Add Game Skeleton component

---
+++ resources/views/components/skeleton-game-card.blade.php
@@ -0,0 +1,12 @@
+<div class='uk-panel uk-box-shadow-medium'>
+    <div class="uk-tile uk-padding-large uk-border-rounded skeleton-image bg-light-gray"></div>
+    <div class="uk-margin-remove-bottom uk-margin-top bg-light-gray text-transparent uk-border-rounded">
+        Game Title
+    </div>
+    <div class='text-transparent uk-margin-small-top uk-border-rounded bg-light-gray uk-width-2-3'>
+        Genre 1, Genre 2
+    </div>
+    <div class='text-transparent uk-margin-small-top uk-border-rounded bg-light-gray uk-width-4-5'>
+        Platform 1 | Platform 2
+    </div>
+</div>
