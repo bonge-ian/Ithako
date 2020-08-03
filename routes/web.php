5fd6cd7fe5d383a5bc3e789f01ab90b52fa75730
Bonge <ikaranjah@zoho.com>
2020-08-03 13:13:43 +0300

    Revert "Add Games Resource"
    This reverts commit eb90fe3e28514dca98d1d9a79e11c08ec7c70a36.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\GamesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [GamesController::class, 'index'])->name('games.index');
+Route::view('/', 'index');
