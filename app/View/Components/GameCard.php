ba2889ca982d08499c97c4fe12541d92e5bcecf3
Bonge <ikaranjah@zoho.com>
2020-08-03 13:12:51 +0300

    Add Game Card Component

--- app/View/Components/GameCard.php
+++ app/View/Components/GameCard.php
@@ -6,31 +6,11 @@ use Illuminate\View\Component;
 
 class GameCard extends Component
 {
-    public string $title;
-    public string $genre;
-    public string $imageUrl;
-    public string $platforms;
-    public float $rating = null;
-
-    /**
-     * Create GameCard Component Instance
-     *
-     * @param string $title
-     * @param string $genre
-     * @param string $platforms
-     * @param string $imageUrl
-     * @param float $rating
-     */
-    public function __construct(
-        string $title, string $genre, string $platforms, string $imageUrl,
-        float $rating  = null
-    )
+    public $game;
+    
+    public function __construct($game)
     {
-        $this->title = $title;
-        $this->genre = $genre;
-        $this->rating = $rating;
-        $this->platforms = $platforms;
-        $this->imageUrl = $imageUrl;
+        $this->game = $game;
     }
 
     /**
---
+++ resources/views/components/game-card.blade.php
@@ -0,0 +1,24 @@
+<div>
+    <a href="#" class="uk-panel uk-display-block uk-link-reset ">
+        <div class="uk-inline uk-transition-toggle " tabindex="0">
+            <img data-src="{{ $game['coverImageUrl'] }}" alt="{{ $game['altText']}}" height="374" width="264" uk-img class='cover-image'>
+            <div
+                class="uk-transition-fade uk-position-cover uk-overlay uk-flex uk-flex-center uk-flex-middle uk-position-small light-overlay">
+            </div>
+            {{-- @if($rating)
+            <div class="rating  uk-position-bottom-right uk-background-secondary uk-border-circle"
+                data-rating="{{ $rating }}">
+            </div>
+            @endif --}}
+        </div>
+        <h3 class="uk-h4 uk-margin-remove-bottom uk-margin-top">{{ $game['name'] }}</h3>
+        @if ($game['genres'])
+        <div class="text-white">
+            {{ $game['genres'] }}
+        </div>
+        @endif
+        <div class="uk-text-meta">
+            {{ $game['platforms'] }}
+        </div>
+    </a>
+</div>
