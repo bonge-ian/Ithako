3d47bb1fd225d7a8db59379c8f79eb40ee5037dd
Bonge <ikaranjah@zoho.com>
2020-08-05 21:19:10 +0300

    Refractor: remove unnecessary white space

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,8 +14,7 @@
     <!-- Styles -->
     <link href="https://fonts.googleapis.com/css2?family=Varta:wght@400;500;700&display=swap" rel="stylesheet">
     <livewire:styles></livewire:styles>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
 <body>
