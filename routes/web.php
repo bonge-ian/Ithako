9d19302ef4147a5393511ce14da6d3a5c39b584e
Bonge <ikaranjah@zoho.com>
2020-08-05 21:21:45 +0300

    Add show route

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\GamesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [GamesController::class, 'index'])->name('games.index');
+Route::get('/{slug}', [GamesController::class, 'show'])->name('games.show');
